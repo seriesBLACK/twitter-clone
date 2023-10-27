b0fb649c08991ba715e107442605006fb6713975
Abd ul Walee Talal <aboodtalal51@gmail.com>
2023-10-27 19:45:28 +0300

    fixed news pug

--- src/components/News.js
+++ src/components/News.js
@@ -1,7 +1,7 @@
 
 export default function News({ articles }) {
   return (
-    <a href={articles.url} target="_blank" className="flex items-center justify-center px-4 py-2 hover:bg-gray-200">
+    <a rel="noreferrer" href={articles.url} target="_blank" className="flex items-center justify-center px-4 py-2 hover:bg-gray-200">
       <div className="space-y-0.5">
         <h6 className="text-sm font-bold">{articles.title}</h6>
         <p className="text-xs font-medium">{articles.source.name}</p>
